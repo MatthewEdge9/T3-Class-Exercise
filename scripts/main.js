bcba202c4e39a46a51ee26a2aeeefd91967b437e
MatthewEdge9 <231173@virtualwindow.co.za>
2023-07-31 01:11:57 +0200

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+// When the document loads
+$(document).ready(function(){
+
+    console.log("Hello");
+
+  
+
+    // -----------------------------------------
+    // Home Page
+
+    // When the document loads, animate the hero image upwards
+    $(".hero-image").animate({top: '-=100px'});
+    
+    $("#ac-1").click(function(){
+      $("#collapse1").collapse('toggle');
+    });
+    $("#ac-2").click(function(){
+      $("#collapse2").collapse('toggle');
+    });
+    $("#ac-3").click(function(){
+      $("#collapse3").collapse('toggle');
+    });
+
+    // -----------------------------------------
+    // Browse Page
+
+    // Hide all description text from the plant cards
+    $("#descriptionText").hide();
+
+  }); 
+
+
+  // When the card is clicked
+  $(".card").click(function(){
+
+    // Toggle the price & description text
+    $("#priceText").toggle();
+    $("#descriptionText").toggle();
+
+    // Resize the image to fit the additional content
+    $(".card-img-top").toggleClass("small");
+
+  });
